a350a4ec5688d965db959aa0b2ff3a03dc9ce698
EduRTorquato <eduardotorquato78@gmail.com>
2023-09-23 13:06:44 -0300

    PI-inicio

---
+++ projetoIntegradorII/src/main/java/initialswingproj/projetointegradorii/ProjetoIntegradorII.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package initialswingproj.projetointegradorii;
+
+/**
+ *
+ * @author eduar
+ */
+public class ProjetoIntegradorII {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
